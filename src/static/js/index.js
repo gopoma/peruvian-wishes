298f3b856365e83a0009e24b29a694159b5a306f
Gustavo <gordono@unsa.edu.pe>
2022-07-11 22:11:59 -0500

    Added dropdown functionality to the Logout

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".message__closer").forEach(message => {
-        message.onclick = function() {
-            message.parentNode.parentNode.removeChild(message.parentNode);
-        }
-    });
+  document.querySelectorAll(".message__closer").forEach(message => {
+    message.onclick = function() {
+      message.parentNode.parentNode.removeChild(message.parentNode);
+    };
+  });
+
+  const profileDropdown = document.querySelector("#profileDropdown");
+  if(profileDropdown) {
+    profileDropdown.onclick = function() {
+      document.querySelector("#btnLogout").classList.toggle("hidden");
+    };
+  }
 });
+
+document.onclick = function(evt) {
+  if(evt.target.id !== "profileDropdown") {
+    document.querySelector("#btnLogout")?.classList.add("hidden");
+  }
+};
